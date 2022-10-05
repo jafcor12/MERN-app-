cf1ba19e4ea247dc7feef57c4cb4a428344ca374
jafcor12 <jafethc12@gmail.com>
2022-10-05 15:49:05 -0600

    Pass variables from the front end to the back end and them to the database

--- client/src/movies.js
+++ client/src/movies.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios'
 import './movies.css'
 
 function Movies() {
 
+    const [movie, setMovie] = useState('');
+    const [review, setReview] = useState('');
+
+    const submitReview = () => {
+        Axios.post("http://localhost:4001/api/insert", {
+                                                         movieName: movie,
+                                                         movieReview: review
+                                                       }
+        ).then(() => {
+            alert("Successful insert");
+        })
+    }
+
     return (
         <div className='form'>
             <label >Movie name</label>
-            <input type="text" name='movieName'/>
+            <input type="text" 
+                   name='movieName' 
+                   onChange={(e) => {
+                       setMovie(e.target.value)
+            }}/>
             <label >Review</label>
-            <input type="text" name='review'/>
+            <input type="text" 
+                   name='review'
+                   onChange={(e) => {
+                       setReview(e.target.value)
+            }}/>
 
-            <button>Submit</button>
+            <button onClick={submitReview}>Submit</button>
         </div>
     )
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,53 @@
 const express = require('express')
 const app = express()
+var snowflake = require('snowflake-sdk');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
 const port = 4001
 
-app.get('/', (req, res) => {
-    res.send('hola')
+var connection = snowflake.createConnection({
+    account: "wdb59468.us-east-1",
+    username: "jafcor06",
+    password: "Hola1234!",
+    database: "MOVIES",
+    warehouse: "my_wh"
+  });
+
+connection.connect( 
+    function(err, conn) {
+        if (err) {
+            console.error('Unable to connect: ' + err.message);
+            } 
+        else {
+            console.log('Successfully connected to Snowflake.');
+            // Optional: store the connection ID.
+            connection_ID = conn.getId();
+            }
+    }
+);
+
+app.use(express.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.post('/api/insert', (req, res) => {
+
+    var movieName = req.body.movieName;
+    var movieReview = req.body.movieReview;
+
+    connection.execute({
+        sqlText: "insert into moviesReview(movieName, movieReview) VALUES (?, ?)",
+        binds: [movieName, movieReview],
+        complete: function(err, stmt, rows) {
+          if (err) {
+            console.error('Failed to execute statement due to the following error: ' + err.message);
+          } else {
+              res.send(`Hola Jafeth has insertado ${rows.length}`);
+          }
+        }
+      });
 })
 
 app.listen(port, () => {
